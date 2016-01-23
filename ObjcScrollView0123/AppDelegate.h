ae5309d812a1d094e75c9439599811d2f78b1ea2
Johnnylkc <momozzz1231@gmail.com>
2016-01-23 20:11:06 +0800

    Initial Commit

---
+++ ObjcScrollView0123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcScrollView0123
+//
+//  Created by 劉坤昶 on 2016/1/23.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcScrollView0123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcScrollView0123
+//
+//  Created by 劉坤昶 on 2016/1/23.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
